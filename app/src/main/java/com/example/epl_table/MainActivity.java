b2a9a742a62bbfcf25cd5d5539ebf0ef86a9e9a9
stephen bett <bettstphen110@gmail.com>
2023-04-17 15:34:43 +0300

    update

--- app/src/main/java/com/example/epl_table/MainActivity.java
+++ app/src/main/java/com/example/epl_table/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView =findViewById(R.id.listView);
+        listView =findViewById(R.id.list_view);
         wordsArray();
         adapter =new MainAdapter(getApplicationContext(),words);
         listView.setAdapter(adapter);
@@ -27,10 +27,27 @@ public class MainActivity extends AppCompatActivity {
 
     private void wordsArray() {
         words =new ArrayList<>();
-        words.add(new WordModal(R.drawable.arsenal,"Arsenal",28,22,3,3,66,26,40,69));
-        words.add(new WordModal(R.drawable.cityz,"Man.City",27,19,4,4,67,25,42,61));
-        words.add(new WordModal(R.drawable.manunt,"Man.Unt",26,15,5,6,52,28,13,50));
-        words.add(new WordModal(R.drawable.tote,"Totenham",28,15,4,9,52,40,12,49));
-        words.add(new WordModal(R.drawable.newc ,"Newcastle",26,12,11,3,39,19,20,47));
+        words.add(new WordModal(R.drawable.arsenal,"Arsenal",31,23,5,3,74,31,43,74));
+        words.add(new WordModal(R.drawable.cityz,"Man.City",30,22,4,4,78,28,50,70));
+        words.add(new WordModal(R.drawable.manunt,"Man.Unt",30,18,5,7,46,37,9,59));
+        words.add(new WordModal(R.drawable.newc ,"Newcastle",30,15,11,4,48,24,24,56));
+        words.add(new WordModal(R.drawable.tote,"Tottenham",28,15,4,9,52,40,12,47));
+        words.add(new WordModal(R.drawable.aston,"Aston Villa",31,15,5,11,44,40,4,50));
+        words.add(new WordModal(R.drawable.brighton,"Brighton",30,15, 11, 6, 41, 35, 6, 50));
+        words.add(new WordModal(R.drawable.liverpool, "Liverpool", 30,15, 11, 6, 41, 35, 6, 50));
+        words.add(new WordModal(R.drawable.brentford, "Brentford", 30,15, 11, 6, 41, 35,6,50));
+        words.add(new WordModal(R.drawable.fullham, "Fulham", 30,15,11,6,41,35,6,50));
+        words.add(new WordModal(R.drawable.chelsea, "Chelsea", 30,15, 11,6,41,35,6,50));
+//        words.add(new WordModal(R.drawable.cy, "Crystal Palace", 30,15,11,6,41,35,6,50));
+//        words.add(new LeagueList(13, R.drawable.wolves, "Wolves", "30","15", "11", "6", "41", "35", "6", "50"));
+//        words.add(new LeagueList(14, R.drawable.bonmouth, "Bournmouth", "30","15", "11", "6", "41", "35", "6", "50"));
+//        words.add(new LeagueList(15, R.drawable.westham, "West Ham", "30","15", "11", "6", "41", "35", "6", "50"));
+//        words.add(new LeagueList(16, R.drawable.leads, "Leads United", "30","15", "11", "6", "41", "35", "6", "50"));
+//        words.add(new LeagueList(17, R.drawable.evaton, "Everton", "30","15", "11", "6", "41", "35", "6", "50"));
+//        words.add(new LeagueList(18, R.drawable.nottm, "Nottm Forest", "30","15", "11", "6", "41", "35", "6", "50"));
+//        words.add(new LeagueList(19, R.drawable.leister, "Leicester City", "30","15", "11", "6", "41", "35", "6", "50"));
+//        words.add(new LeagueList(20, R.drawable.southampton, "Southampton", "30","15", "11", "6", "41", "35", "6", "50"));
+
+
     }
 }
--- app/src/main/java/com/example/epl_table/MainAdapter.java
+++ app/src/main/java/com/example/epl_table/MainAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -35,42 +36,66 @@ public class MainAdapter extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         view = LayoutInflater.from(context).inflate(R.layout.standings,viewGroup, false);
-//       position =view.findViewById(R.id.pos);
-       imgArsenal =view.findViewById(R.id.Imgarsenal);
-       formwon =view.findViewById(R.id.fomwin);
-       formlos =view.findViewById(R.id.fomlos);
-       formwon2 =view.findViewById(R.id.fomwin2);
-       formdown =view.findViewById(R.id.fomdown);
-       formwon3 =view.findViewById(R.id.fomwin3);
+
+//        clubName, played, won, drawn, lost,goalF,goalA,goalD,totalPoints;
+//        TextView teamNumber = view.findViewById(R.id.team_id);
+        imgArsenal = view.findViewById(R.id.team_image_id);
+        clubName = view.findViewById(R.id.team_name_id);
+        played = view.findViewById(R.id.mp_id);
+        won = view.findViewById(R.id.wins_id);
+        drawn = view.findViewById(R.id.draws_id);
+        lost = view.findViewById(R.id.loose_id);
+        goalF= view.findViewById(R.id.goalForward_id);
+        goalA= view.findViewById(R.id.goalAgainst_id);
+        goalD= view.findViewById(R.id.goalDifference_id);
+        totalPoints= view.findViewById(R.id.points_id);
 
 
-       clubName =view.findViewById(R.id.txtArsenal);
-       played = view.findViewById(R.id.played);
-       won =view.findViewById((R.id.gwon));
-       drawn =view.findViewById(R.id.gdraw);
-       lost =view.findViewById(R.id.glost);
-       goalF =view.findViewById(R.id.goalF);
-       goalA =view.findViewById(R.id.goalA);
-       goalD =view.findViewById(R.id.goalD);
-       totalPoints =view.findViewById(R.id.Total_points);
+        imgArsenal.setImageResource(words.get(i).getClubImage());
+        clubName.setText(words.get(i).getClubName());
+        played.setText(Integer.toString(words.get(i).getPlayed()));
+        won.setText(Integer.toString(words.get(i).getWon()));
+        drawn.setText(Integer.toString(words.get(i).getDrawn()));
+        lost.setText(Integer.toString(words.get(i).getLost()));
+        goalF.setText(Integer.toString(words.get(i).getgF()));
+        goalA.setText(Integer.toString(words.get(i).getgA()));
+        goalD.setText(Integer.toString(words.get(i).getgD()));
+        totalPoints.setText(Integer.toString(words.get(i).getPoints()));
 
-
-       imgArsenal.setImageResource(words.get(i).getClubImage());
-       clubName.setText(words.get(i).getClubName());
-       played.setText(Integer.toString(words.get(i).getPlayed()));
-       won.setText(Integer.toString(words.get(i).getWon()));
-       drawn.setText(Integer.toString(words.get(i).getDrawn()));
-       lost.setText(Integer.toString(words.get(i).getLost()));
-       goalF.setText(Integer.toString(words.get(i).getgF()));
-       goalA.setText(Integer.toString(words.get(i).getgA()));
-       goalD.setText(Integer.toString(words.get(i).getgD()));
-       totalPoints.setText(Integer.toString(words.get(i).getPoints()));
-//       formwon.setImageResource(words.get(i).get);
-
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Position" + i, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return  view;
     }
 
-
-
 }
+//       position =view.findViewById(R.id.pos);
+//       imgArsenal =view.findViewById(R.id.Imgarsenal);
+//       formwon =view.findViewById(R.id.fomwin);
+//       formlos =view.findViewById(R.id.fomlos);
+//       formwon2 =view.findViewById(R.id.fomwin2);
+//       formdown =view.findViewById(R.id.fomdown);
+//       formwon3 =view.findViewById(R.id.fomwin3);
+//
+//
+//       clubName =view.findViewById(R.id.txtArsenal);
+//       played = view.findViewById(R.id.played);
+//       won =view.findViewById((R.id.gwon));
+//       drawn =view.findViewById(R.id.gdraw);
+//       lost =view.findViewById(R.id.glost);
+//       goalF =view.findViewById(R.id.goalF);
+//       goalA =view.findViewById(R.id.goalA);
+//       goalD =view.findViewById(R.id.goalD);
+//       totalPoints =view.findViewById(R.id.Total_points);
+//
+
+//
+
+//       formwon.setImageResource(words.get(i).get);
+
+
+
